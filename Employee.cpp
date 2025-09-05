eb439a894741f47a9d34b473a854fe75a64ba467
Luiezequiel <luiezequiel.benitez@upr.edu>
2025-09-05 18:00:30 +0000

    employee cpp

--- Employee.cpp
+++ Employee.cpp
@@ -4,6 +4,14 @@ Employee::Employee(const std::string& name, int id, const std::string& type,
                    double payPerHour, double workedHours, double yearlyPayment)
 {
   // TODO: 
+  name = name;
+  id = id;
+  type = type;
+  payPerHour = payPerHour;
+  yearlyPayment = yearlyPayment;
 }
 
 /* IMPLEMENT GETTERS*/
+Employee::getType()const{return type;}
+
+Employee::getName(){return name;}
