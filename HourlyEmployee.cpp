16312ffe457ddec1dc5713a442d0a01d3dbf0d42
Luiezequiel <luiezequiel.benitez@upr.edu>
2025-09-17 03:39:08 +0000

    Ya termine los todo

--- HourlyEmployee.cpp
+++ HourlyEmployee.cpp
@@ -4,6 +4,19 @@ HourlyEmployee::HourlyEmployee(const std::string& name, int id,
                                 double payPerHour, double workedHours)
 {
     // TODO: 
+    name = name;          // guardo el nombre
+    id = id;            // guardo el id
+    payPerHour = payPerHour;    // guardo el pago por hora
+    workedHours = workedHours;
 }
 /*TODO: calculate Payment */
+
+HourlyEmployee::getPayPerHour() const{return payPerHour;}
+HourlyEmployee::getPayPerHour() const {return workedHours;}
+
+HourlyEmployee::calculatePay(){
+    return getPayPerHour() * getWorkedHours();
+}
+
+
 
